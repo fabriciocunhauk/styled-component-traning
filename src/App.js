10550fc783ca05ddddf99b2939d53a20d0c43164
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-12-15 04:00:36 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import styled from 'styled-components';
+
+import Card from './components/Card/Card.component';
+
 import './App.css';
+
+const Text = styled.div`
+  color: red;
+  font-size: 20px;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card>
+        <Text>Im a component</Text>
+      </Card>
     </div>
   );
 }
